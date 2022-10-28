9476ab1337c156661fc9aa3c9ad325c76e49d2ef
Kin Lane <kinlane@gmail.com>
2022-10-28 14:07:36 -0700

    add tags

---
+++ videos/video_id/tags/post/index.js
@@ -0,0 +1,44 @@
+const vandium = require('vandium');
+const mysql  = require('mysql');
+
+exports.handler = vandium.generic()
+  .handler( (event, context, callback) => {
+
+    var connection = mysql.createConnection({
+    host     : process.env.host,
+    user     : process.env.user,
+    password : process.env.password,
+    database : process.env.database
+    });
+
+
+    connection.connect(function(err) {
+      if (err) throw err;
+
+      var sql = "SELECT * FROM videos_tags WHERE video_id = " + event.video_id + " and tag_id = " + event.tag_id + " LIMIT 1";
+      connection.query(sql, function (err, result, fields) {
+        if (err) throw err;
+        if(result && result.length > 0){
+          callback( null, result[0] ); 
+          connection.end();
+        }
+        else{
+
+          var sql = "INSERT INTO videos_tags(video_id,tag_id) VALUES(" + event.video_id + ", " + event.tag_id + ")";
+        
+          connection.query(sql, function (error, results, fields) {
+
+            var inserted = {};
+            inserted.id = results.insertId;
+            inserted.video_id = event.video_id;
+            inserted.tag_id = event.tag_id;
+        
+            callback( null, inserted );
+            connection.end();
+      
+          });          
+
+        }
+      });
+    });
+});
